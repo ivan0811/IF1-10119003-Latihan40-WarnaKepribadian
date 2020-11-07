dd56d35a28af47af35ba1c74e5176eef046c1c83
Ivan Faathirza <49663880+ivan0811@users.noreply.github.com>
2020-11-07 16:53:23 +0700

    initial commit

--- src/com/ivanfaathirza/warnakepribadian/Latihan40WarnaKepribadian.java
+++ src/com/ivanfaathirza/warnakepribadian/Latihan40WarnaKepribadian.java
@@ -2,6 +2,16 @@ package com.ivanfaathirza.warnakepribadian;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author Ivan
+ * NAMA         : Ivan Faathirza
+ * KELAS        : IF1
+ * NIM          : 10119003
+ * Deskripsi Program : program ini untuk mencari kepribadian berdasarkan warna dari masukkan user, berbasis objek dengan 
+ * instansiasi objek user
+ */
+
 public class Latihan40WarnaKepribadian {
     public static void main(String[] args) {
         User user = new User();
